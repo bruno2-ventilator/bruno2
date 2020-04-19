fe83779f7d9c87cea4faa47b0b361df0c27e218d
jrosenstein <jacob_rosenstein@brown.edu>
2020-04-19 15:04:14 -0400

    cleanup

--- Design Materials/Software/raspi/startBrunO2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#start screen session
-NL=`echo -ne '\015'`
-screen -dmS bruno2App -t runTime
-screen -S bruno2App -X screen -t nav
-screen -S bruno2App -p runTime -X stuff "cd ~/bruno2/raspi/$NL"
-screen -S bruno2App -p runTime -X stuff "python3 main_basic.py$NL"
-#screen -S bruno2App -p runTime -X stuff "python3 main_basic.py /dev/ttyACM0$NL"
